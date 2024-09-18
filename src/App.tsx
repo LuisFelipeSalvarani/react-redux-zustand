caf3b85f2db99def614c5a7a0b303cb6a18a7680
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-09-18 20:05:49 -0300

    feat:começando um novo aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { Provider as ReduxProvider } from 'react-redux'
 
-import { AddTodo } from './components/AddTodo'
-import { TodoList } from './components/TodoList'
 import { store } from './store'
 
 export function App() {
-  return (
-    <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
-    </ReduxProvider>
-  )
+  return <ReduxProvider store={store}></ReduxProvider>
 }
