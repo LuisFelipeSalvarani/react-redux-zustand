4adce37e1e1df3b77a82b6d76a9e6b8f990a37e2
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-09-18 21:48:24 -0300

    chore: configuração inicial do tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import './styles/global.css'
+
 import { Provider as ReduxProvider } from 'react-redux'
 
+import { Player } from './pages/Player'
 import { store } from './store'
 
 export function App() {
-  return <ReduxProvider store={store}></ReduxProvider>
+  return (
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
+  )
 }
---
+++ src/pages/Player.tsx
@@ -0,0 +1,3 @@
+export function Player() {
+  return <h1 className="font-medium">Player</h1>
+}
